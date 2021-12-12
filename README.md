1c75c548e51b08b72d8107094f26ebf0e4fce8b8
Andreas Schmid <andreas.schmid@ur.de>
2021-12-12 19:51:29 +0100

    add preview image and script to create said preview image

---
+++ README.md
@@ -0,0 +1,7 @@
+# Free Graphics by Andreas Schmid
+
+All of the graphics in this repository were created by me and are licensed CC0 - do whatever you want with them.
+
+<p align="center">
+  <img src="preview.png" width=500>
+</p>
---
+++ create_previews.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir tmp
+
+COUNTER=0
+
+# iterate all svg files in all subdirectories
+for file in $(find . -iname '*.svg');
+do
+    # use inkscape to convert svg to png and store results in temporary directory
+    inkscape -w 256 $file -o "tmp/$COUNTER.png"
+    COUNTER=$((COUNTER+1))
+done;
+
+# use imagemagick to create a montage of all images in the repository
+montage "tmp/*.png" -tile "4x$(((COUNTER/4)+1))" preview.png
+
+rm -r tmp
